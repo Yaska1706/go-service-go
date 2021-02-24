5afe775d6f291eb73219283b1ebc4b3b458965b7
mbv <heymbv@gmail.com>
2021-02-24 22:44:55 +0100

    main.go in a good state now

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,29 +2,76 @@ package main
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"go-mbv-go/pkg/api"
 	"go-mbv-go/pkg/http"
 	"go-mbv-go/pkg/repository"
+	"os"
 )
 
 func main() {
-	connectionString := "postgres://postgres:postgres@localhost/**NAME-OF-YOUR-DATABASE-HERE**?sslmode=disable"
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "this is the startup error: %s\n", err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	port := os.Getenv("PORT")
+	if port == "" {
+		return errors.New("port was not specified")
+	}
+
+	db, err := setupDatabase()
+	if err != nil {
+		return err
+	}
+
+	// TODO: rename this service, only placeholder
+	placeholderService := api.NewUserService(db)
+
+	// setup the api
+	api := api.NewApi(placeholderService)
+
+	// spin up the server
+	srv, err := setupRouter(api)
+	if err != nil {
+		return err
+	}
+
+	return srv.Run(":" + port)
+}
+
+func setupDatabase() (repository.Storage, error) {
+	connectionString := os.Getenv("DATABASE_URL")
 	db, err := sql.Open("postgres", connectionString)
 	if err != nil {
-		fmt.Printf("this is db open error: %v", err)
+		return nil, err
 	}
-	storage := repository.NewStorage(db)
 
-	userService := api.NewUserService(storage)
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
 
-	api := api.NewApi(userService)
+	return repository.NewStorage(db), err
+}
 
-	// create router dependency
-	router := gin.Default()
-	srv := http.NewRouter(router, api)
+// TODO: fix this naming at one point api.API looks stupid
+func setupRouter(api api.API) (http.Router, error) {
+		// create router dependency
+	gin.SetMode(gin.ReleaseMode)
+	env := os.Getenv("ENVIRONMENT")
 
-	// start server
-	srv.Run(":8000")
+	if env == "" {
+		return nil, errors.New("env was not specified")
+	}
+
+	if env == "DEVELOPMENT" {
+		gin.SetMode(gin.DebugMode)
+	}
+		router := gin.Default()
+		return http.NewRouter(router, api), nil
 }

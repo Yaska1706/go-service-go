3506f2a2edc730d5dd3500a460684a0cbdf1b44a
mbv <heymbv@gmail.com>
2021-02-27 14:28:22 +0100

    clean up

--- pkg/repository/postgres.go
+++ pkg/repository/postgres.go
@@ -2,50 +2,31 @@ package repository
 
 import (
 	"database/sql"
-	"errors"
 	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 	_ "github.com/lib/pq"
 	"go-mbv-go/internal"
+	"os"
 	"path/filepath"
 )
 
 type Storage interface {
-	RunMigrations(connectionString string) error
+	Migration
 }
 
 type storage struct {
-	db *sql.DB
+	db        *sql.DB
+	migration *migrate.Migrate
 }
 
 func NewStorage(db *sql.DB) Storage {
-	return &storage{db: db}
-}
-
-func (s storage) RunMigrations(connectionString string) error {
-	if connectionString == "" {
-		return errors.New("repository: the connectionString was empty")
-	}
-
 	basePath := internal.GetBasePath()
-
-	migrationsPath := filepath.Join("file://", basePath, "/migrations")
-
-	m, err := migrate.New(migrationsPath, connectionString)
-
+	migrationsDir := filepath.Join("file://", basePath, "/migrations")
+	migrate, err := migrate.New(migrationsDir, os.Getenv("DATABASE_URL"))
 	if err != nil {
-		return err
+		panic("could not setup migration for Storage")
 	}
 
-	err = m.Up()
-
-	if err != nil {
-		if err.Error() == "no change" {
-			return nil
-		}
-		return err
-	}
-
-	return nil
+	return &storage{db: db, migration: migrate}
 }
